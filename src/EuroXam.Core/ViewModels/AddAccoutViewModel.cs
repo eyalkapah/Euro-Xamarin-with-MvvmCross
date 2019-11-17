f11d80690464072b609faecee6547704946518aa
Eyal.Kapah <eykapah@microsoft.com>
2019-11-17 08:12:59 +0200

    push icons

--- src/EuroXam.Core/ViewModels/AddAccoutViewModel.cs
+++ src/EuroXam.Core/ViewModels/AddAccoutViewModel.cs
@@ -10,10 +10,16 @@ namespace EuroXam.Core.ViewModels
     public class AddAccoutViewModel : MvxNavigationViewModel
     {
         public ICommand LoginWithFacebookCommand { get; set; }
+        public ICommand LoginWithGoogleCommand { get; set; }
 
         public AddAccoutViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
         {
             LoginWithFacebookCommand = new MvxCommand(LoginWithFacebook);
+            LoginWithGoogleCommand = new MvxCommand(LoginWithGoogle);
+        }
+
+        private void LoginWithGoogle()
+        {
         }
 
         private void LoginWithFacebook()
